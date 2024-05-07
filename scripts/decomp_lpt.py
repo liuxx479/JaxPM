aec1f208c1fc172eb0d46077bec080d65bd0fdcd
Wassim Kabalan <waston@gmail.com>
2024-05-07 21:22:01 +0200

    Adjusted for new version of jaxDecomp (branch fix_fft)

--- scripts/decomp_lpt.py
+++ scripts/decomp_lpt.py
@@ -73,17 +73,17 @@ size = jax.process_count()
 master_key = jax.random.PRNGKey(42)
 key = jax.random.split(master_key, size)[rank]
 
-pdims = (4, 1)
-mesh_shape = [512, 512, 512]
+pdims = (2 , 2)
+mesh_shape = [256, 256, 256]
 box_size = [200, 200, 200]  # Mpc/h
 
 # Create computing mesgh
-devices = mesh_utils.create_device_mesh(pdims[::-1])
-mesh = Mesh(devices, axis_names=('x', 'y'))
-sharding = jax.sharding.NamedSharding(mesh, P('y', 'x'))
+devices = mesh_utils.create_device_mesh(pdims)
+mesh = Mesh(devices, axis_names=('y', 'z'))
+sharding = jax.sharding.NamedSharding(mesh, P('z', 'y'))
 
 ### Create all initial distributed tensors ###
-local_mesh_shape = [mesh_shape[0]//pdims[0], mesh_shape[1]//pdims[1], mesh_shape[2]]
+local_mesh_shape = [mesh_shape[0]//pdims[1], mesh_shape[1]//pdims[0], mesh_shape[2]]
 
 # Create gaussian field distributed across the mesh
 z = jax.make_array_from_single_device_arrays(shape=mesh_shape,
@@ -91,27 +91,30 @@ z = jax.make_array_from_single_device_arrays(shape=mesh_shape,
                                              arrays=[jax.random.normal(key, local_mesh_shape)])
 
 # I'm not sure I understand the order of the data I have to provide here
+# Order is ZXY column major so YXZ
 pos = jax.make_array_from_callback(shape=tuple(mesh_shape+[3]),
                                    sharding=sharding,
                                    data_callback=lambda x: jnp.stack(jnp.meshgrid(jnp.arange(mesh_shape[1])[x[1]],
-                                                                                  jnp.arange(mesh_shape[0])[x[0]], 
-                                                                                  jnp.arange(mesh_shape[2])), axis=-1))
+                                                                                  jnp.arange(mesh_shape[2])[x[0]], 
+                                                                                  jnp.arange(mesh_shape[0])), axis=-1))
 
-kd = np.fft.fftfreq(mesh_shape[0]) * 2 * np.pi
+# this is a cube so mesh shape doesn't matter
+# but I think it should be 0 1 2 and 2 for X
+kd = np.fft.fftfreq(mesh_shape[0]) * 2 * np.pi # must be mesh_shape[2]
 kvec = [jax.make_array_from_callback((mesh_shape[0], 1, 1), 
-                                    sharding=jax.sharding.NamedSharding(mesh, P('y')),
+                                    sharding=jax.sharding.NamedSharding(mesh, P('z')),
                                     data_callback=lambda x: kd.reshape([-1,1,1])[x]),
         jax.make_array_from_callback((1, mesh_shape[1], 1), 
-                                    sharding=jax.sharding.NamedSharding(mesh, P(None, 'x')),
+                                    sharding=jax.sharding.NamedSharding(mesh, P(None, 'y')),
                                     data_callback=lambda x: kd.reshape([1,-1,1])[x]),
         kd.reshape([1,1,-1])]
 
 # Checking the size of all local tensors
 print('rank =', rank, 'z.local_shape =', 
-      z.addressable_shards[0].data.shape,
-      'pos.local_shape =', pos.addressable_shards[0].data.shape,
-      'kvec[0].local_shape =', kvec[0].addressable_shards[0].data.shape,
-      'kvec[1].local_shape =', kvec[1].addressable_shards[0].data.shape)
+      z.addressable_data(0).shape,
+      'pos.local_shape =', pos.addressable_data(0).shape,
+      'kvec[0].local_shape =', kvec[0].addressable_data(0).shape,
+      'kvec[1].local_shape =', kvec[1].addressable_data(0).shape)
 #############################################
 
 @jax.jit
@@ -121,29 +124,34 @@ def forward_fn(z, kvec, pos, a):
     # Rescaling k to physical units
     kvec = [k / box_size[i] * mesh_shape[i]
             for i, k in enumerate(kvec)]
-    kx, ky, kz = kvec
+    kz, ky, kx = kvec
     kk = jnp.sqrt(kx**2+ky**2+kz**2)
     
-    k = jnp.logspace(-4, 2, 256)
+    k = jnp.logspace(-4, 2, 256) # I don't understand why 256?
     pk = jc.power.linear_matter_power(jc.Planck15(), k)
     pk = pk * (mesh_shape[0] * mesh_shape[1] * mesh_shape[2]
                 ) / (box_size[0] * box_size[1] * box_size[2])
 
     # Multipliyng the field by the proper power spectrum
+    # Interpolate has a vmap in it ... so it is all-gathered since jax does not know how to deal with it
+    # it should probably be redone (not just shard-map because it still all-gather)
     delta_k = kfield * jc.scipy.interpolate.interp(kk.flatten(), k, jnp.sqrt(pk)).reshape(kfield.shape)
 
     # Inverse fourier transform to generate the initial conditions
     initial_conditions = jaxdecomp.fft.pifft3d(delta_k).real
+    # Now the result is an X pencil so ZYX for us
 
     ###  Compute LPT displacement
     kernel = jnp.where(kk == 0, 1., 1./kk) # Laplace kernel
-    forces_k = jnp.stack([delta_k * kernel * 1j * 1 / 6.0 * (8 * jnp.sin(kz) - jnp.sin(2 * kz)),
+    # Forces have to be a Z pencil because they are going to be IFFT back to X pencil
+    forces_k = jnp.stack([delta_k * kernel * 1j * 1 / 6.0 * (8 * jnp.sin(ky) - jnp.sin(2 * ky)),
                           delta_k * kernel * 1j * 1 / 6.0 * (8 * jnp.sin(kx) - jnp.sin(2 * kx)),
-                          delta_k * kernel * 1j * 1 / 6.0 * (8 * jnp.sin(ky) - jnp.sin(2 * ky))], 
+                          delta_k * kernel * 1j * 1 / 6.0 * (8 * jnp.sin(kz) - jnp.sin(2 * kz))], 
                           axis=-1)
     
     # Interpolate forces at the position of particles
     pos = pos.reshape([-1,3])
+    # I don't understand this very well .. three IFFTs is ok I understand (for each direction) but each on a k ? each k is 3D object?
     initial_force = jnp.stack([cic_read(jaxdecomp.fft.pifft3d(forces_k[..., i]).real, pos)
                                for i in range(3)], axis=-1)
     cosmo = jc.Planck15()
